0650aec4c8fd24ab1dbb1b379581f1d2806fe430
Tim Pelgrim <t.pelgrim@yipyip.nl>
2018-09-23 16:27:28 +0200

    updated version number to 0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Slack.Mixfile do
 
   def project do
     [app: :slack,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
